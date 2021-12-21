2ff14f8d537a2e09144cf80722c4264f4d6620f1
Benjamin Kurrek <bkurrek@uwaterloo.ca>
2021-12-21 10:19:58 -0500

    Added promise or value to imports

--- nft-contract/src/lib.rs
+++ nft-contract/src/lib.rs
@@ -4,7 +4,7 @@ use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::json_types::{Base64VecU8, U128};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
-    env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise,
+    env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, PromiseOrValue,
 };
 
 pub use crate::metadata::*;
