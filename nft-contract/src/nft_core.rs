adc039ee56765ad478bf56861a42d122c70658b4
Benjamin Kurrek <bkurrek@uwaterloo.ca>
2022-01-07 18:32:23 -0500

    Added more GAS for the NFT transfer call

--- nft-contract/src/nft_core.rs
+++ nft-contract/src/nft_core.rs
@@ -2,7 +2,7 @@ use crate::*;
 use near_sdk::{ext_contract, Gas, PromiseResult};
 
 const GAS_FOR_RESOLVE_TRANSFER: Gas = Gas(10_000_000_000_000);
-const GAS_FOR_NFT_TRANSFER_CALL: Gas = Gas(5_000_000_000_000 + GAS_FOR_RESOLVE_TRANSFER.0);
+const GAS_FOR_NFT_TRANSFER_CALL: Gas = Gas(25_000_000_000_000 + GAS_FOR_RESOLVE_TRANSFER.0);
 const MIN_GAS_FOR_NFT_TRANSFER_CALL: Gas = Gas(100_000_000_000_000);
 const NO_DEPOSIT: Balance = 0;
 
