87f54f9ebf07c0e54ffd7eba1a3912f76f712df6
BenKurrek <bkurrek@uwaterloo.ca>
2022-01-13 17:22:23 -0500

    Added total supply as part of the enumeration standard

--- nft-contract/src/enumeration.rs
+++ nft-contract/src/enumeration.rs
@@ -2,6 +2,12 @@ use crate::*;
 
 #[near_bindgen]
 impl Contract {
+    //Query for the total supply of NFTs on the contract
+    pub fn nft_total_supply(&self) {
+        /*
+            FILL THIS IN
+        */
+    }
 
     //Query for nft tokens on the contract regardless of the owner using pagination
     pub fn nft_tokens(&self, from_index: Option<U128>, limit: Option<u64>) {
