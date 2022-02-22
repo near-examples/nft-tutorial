f9ce6878db1acc857b0d50ec519891991aa52119
BenKurrek <bkurrek@uwaterloo.ca>
2022-02-22 11:04:56 -0700

    Changed token ID param type to TokenId as per pythonicode suggestion

--- nft-contract/src/royalty.rs
+++ nft-contract/src/royalty.rs
@@ -2,7 +2,7 @@ use crate::*;
 
 pub trait NonFungibleTokenCore {
     //calculates the payout for a token given the passed in balance. This is a view method
-  	fn nft_payout(&self, token_id: String, balance: U128, max_len_payout: u32);
+  	fn nft_payout(&self, token_id: TokenId, balance: U128, max_len_payout: u32);
     
     //transfers the token to the receiver ID and returns the payout object that should be payed given the passed in balance. 
     fn nft_transfer_payout(
@@ -20,7 +20,7 @@ pub trait NonFungibleTokenCore {
 impl NonFungibleTokenCore for Contract {
 
     //calculates the payout for a token given the passed in balance. This is a view method
-    fn nft_payout(&self, token_id: String, balance: U128, max_len_payout: u32) {
+    fn nft_payout(&self, token_id: TokenId, balance: U128, max_len_payout: u32) {
 		/*
             FILL THIS IN
         */
