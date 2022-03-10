a5cfe2bcc16e99fbe25cb9157ee78edb2f832bf5
Dennis <10233439+idea404@users.noreply.github.com>
2022-03-10 15:07:40 +0000

    unit test for internal transfer

--- nft-contract/src/tests.rs
+++ nft-contract/src/tests.rs
@@ -2,14 +2,14 @@
 #[cfg(test)]
 use crate::Contract;
 use crate::TokenMetadata;
-use near_sdk::json_types::U128;
+use near_sdk::json_types::{U128, U64};
 use near_sdk::test_utils::{accounts, VMContextBuilder};
 use near_sdk::testing_env;
 use near_sdk::{env, AccountId};
 
 use std::collections::HashMap;
 
-const MINT_STORAGE_COST: u128 = 100000000000000000000000;
+const MINT_STORAGE_COST: u128 = 100_000_000_000_000_000_000_000;
 
 fn get_context(predecessor: AccountId) -> VMContextBuilder {
     let mut builder = VMContextBuilder::new();
@@ -78,5 +78,61 @@ fn test_mint_nft() {
         contract_nft_tokens[0].metadata.description,
         sample_token_metadata().description
     );
+    assert_eq!(
+        contract_nft_tokens[0].metadata.media,
+        sample_token_metadata().media
+    );
     assert_eq!(contract_nft_tokens[0].approved_account_ids, HashMap::new());
+}
+
+#[test]
+fn test_internal_transfer() {
+    let mut context = get_context(accounts(0));
+    testing_env!(context.build());
+    let mut contract = Contract::new_default_meta(accounts(0).into());
+
+    testing_env!(context
+        .storage_usage(env::storage_usage())
+        .attached_deposit(MINT_STORAGE_COST)
+        .predecessor_account_id(accounts(0))
+        .build());
+    let token_id = "0".to_string();
+    contract.nft_mint(token_id.clone(), sample_token_metadata(), accounts(0), None);
+
+    testing_env!(context
+        .storage_usage(env::storage_usage())
+        .attached_deposit(1)
+        .predecessor_account_id(accounts(0))
+        .build());
+    contract.internal_transfer(
+        &accounts(0),
+        &accounts(1),
+        &token_id.clone(),
+        Some(U64(1).0),
+        None,
+    );
+
+    testing_env!(context
+        .storage_usage(env::storage_usage())
+        .account_balance(env::account_balance())
+        .is_view(true)
+        .attached_deposit(0)
+        .build());
+
+    let tokens = contract.nft_tokens_for_owner(accounts(1), Some(U128(0)), None);
+    assert_ne!(
+        tokens.len(),
+        0,
+        "Token not correctly created and/or sent to second account"
+    );
+    let token = &tokens[0];
+    assert_eq!(token.token_id, token_id);
+    assert_eq!(token.owner_id, accounts(1));
+    assert_eq!(token.metadata.title, sample_token_metadata().title);
+    assert_eq!(
+        token.metadata.description,
+        sample_token_metadata().description
+    );
+    assert_eq!(token.metadata.media, sample_token_metadata().media);
+    assert_eq!(token.approved_account_ids, HashMap::new());
 }
