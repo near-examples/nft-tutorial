17621e3cae97774258a8b30bf4c17edf42c34066
Dennis <10233439+idea404@users.noreply.github.com>
2022-03-14 14:58:35 +0000

    storage_balance_of test

--- market-contract/src/tests.rs
+++ market-contract/src/tests.rs
@@ -4,7 +4,7 @@ use crate::Contract;
 use near_sdk::{
     env,
     test_utils::{accounts, VMContextBuilder},
-    testing_env, AccountId,
+    testing_env, AccountId, json_types::U128,
 };
 
 const MIN_REQUIRED_APPROVAL_YOCTO: u128 = 170000000000000000000;
@@ -52,4 +52,19 @@ fn test_storage_deposit() {
     let outcome = contract.storage_deposits.get(&accounts(0));
     let expected = MIN_REQUIRED_STORAGE_YOCTO;
     assert_eq!(outcome, Some(expected));
+}
+
+#[test]
+fn test_storage_balance_of() {
+    let mut context = get_context(accounts(0));
+    testing_env!(context.build());
+    let mut contract = Contract::new(accounts(0));
+    testing_env!(context
+        .storage_usage(env::storage_usage())
+        .attached_deposit(MIN_REQUIRED_STORAGE_YOCTO)
+        .predecessor_account_id(accounts(0))
+        .build());
+    contract.storage_deposit(Some(accounts(0)));
+    let balance = contract.storage_balance_of(accounts(0));
+    assert_eq!(balance, U128(MIN_REQUIRED_STORAGE_YOCTO));
 }
