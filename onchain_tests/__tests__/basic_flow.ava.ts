72f1da38a606852ba3cef684670e384e16a4f04e
Dennis <10233439+idea404@users.noreply.github.com>
2022-03-28 15:28:10 +0100

    add sim test for minting NFT

--- onchain_tests/__tests__/basic_flow.ava.ts
+++ onchain_tests/__tests__/basic_flow.ava.ts
@@ -1,7 +1,10 @@
 import { Workspace } from "near-workspaces-ava";
+import { BN } from "near-workspaces";
 
 const workspace = Workspace.init(async ({ root }) => {
   const alice = await root.createAccount("alice");
+  const bob = await root.createAccount("bob");
+  const charlie = await root.createAccount("charlie");
 
   const main_contract = await root.createAndDeploy(
     "nft-contract",
@@ -12,25 +15,20 @@ const workspace = Workspace.init(async ({ root }) => {
     }
   );
 
-  // await alice.call(main_contract, 'set_status', {message: 'hello'});
-
   const market_contract = await root.createAndDeploy(
     "nft-market",
-    "../out/market.wasm"
-
-    // TODO
-    // Provide `method` and `args` to call in the same transaction as the deploy
-    // {
-    //   method: 'init',
-    //   args: {owner_id: root},
-    // },
+    "../out/market.wasm",
+    {
+      method: "new",
+      args: { owner_id: root },
+    }
   );
 
-  return { alice, main_contract, market_contract };
+  return { alice, bob, charlie, main_contract, market_contract };
 });
 
 workspace.test(
-  "main contract: nft metadata view call",
+  "main contract: nft metadata view",
   async (test, { main_contract, root }) => {
     const expected = {
       base_uri: null,
@@ -45,5 +43,54 @@ workspace.test(
       await main_contract.view("nft_metadata", { account_id: root }),
       expected
     );
+  }
+);
+
+workspace.test(
+  "main contract: nft mint call",
+  async (test, { main_contract, alice, root }) => {
+    const request_payload = {
+      token_id: "TEST123",
+      metadata: {
+        title: "GO TEAM",
+        description: "Alright time's up, let's do this.",
+        media:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Fhp4lHufCdTzTeGCAblOdgHaF7%26pid%3DApi&f=1",
+      },
+      receiver_id: alice,
+    };
+    test.log("Request payload: ", request_payload);
+    const options = {
+      gas: new BN("75000000000000"), // min gas: https://stackoverflow.com/questions/70088651/near-executionerrorexceeded-the-prepaid-gas
+      attachedDeposit: new BN("7880000000000000000001"), // Must attach 7880000000000000000000 yoctoNEAR to cover storage
+    };
+    test.log("Options: ", options);
+    await root.call(main_contract, "nft_mint", request_payload, options);
+
+    const tokens = await main_contract.view("nft_tokens");
+    const expected = [
+      {
+        approved_account_ids: {},
+        metadata: {
+          copies: null,
+          description: "Alright time's up, let's do this.",
+          expires_at: null,
+          extra: null,
+          issued_at: null,
+          media:
+            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Fhp4lHufCdTzTeGCAblOdgHaF7%26pid%3DApi&f=1",
+          media_hash: null,
+          reference: null,
+          reference_hash: null,
+          starts_at: null,
+          title: "GO TEAM",
+          updated_at: null,
+        },
+        owner_id: "alice.test.near",
+        royalty: {},
+        token_id: "TEST123",
+      },
+    ];
+    test.deepEqual(tokens, expected, "Expected to find one minted NFT");
   }
 );
