b505bcfc0a751b4fdfb9fe0e050d375dbe91dcf1
BenKurrek <bkurrek@uwaterloo.ca>
2022-06-21 12:23:50 -0400

    revert nft core update

--- nft-contract/src/nft_core.rs
+++ nft-contract/src/nft_core.rs
@@ -88,10 +88,19 @@ impl NonFungibleTokenCore for Contract {
 
     //get the information for a specific token ID
     fn nft_token(&self, token_id: TokenId) -> Option<JsonToken> {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+        //if there is some token ID in the tokens_by_id collection
+        if let Some(token) = self.tokens_by_id.get(&token_id) {
+            //we'll get the metadata for that token
+            let metadata = self.token_metadata_by_id.get(&token_id).unwrap();
+            //we return the JsonToken (wrapped by Some since we return an option)
+            Some(JsonToken {
+                token_id,
+                owner_id: token.owner_id,
+                metadata,
+            })
+        } else { //if there wasn't a token ID in the tokens_by_id collection, we return None
+            None
+        }
     }
 }
 
