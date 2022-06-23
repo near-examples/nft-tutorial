3226a1965ba14e30afff1c708ceb62590e909eb0
Dennis <10233439+idea404@users.noreply.github.com>
2022-06-23 16:40:23 +0100

    add nft int tests

--- integration-tests/ts/src/main.ava.ts
+++ integration-tests/ts/src/main.ava.ts
@@ -1,5 +1,6 @@
 import { Worker, NearAccount, NEAR } from 'near-workspaces';
 import anyTest, { TestFn } from 'ava';
+import { defaultCallOptions, mintNFT, approveNFT } from "./utils";
 
 const test = anyTest as TestFn<{
     worker: Worker;
@@ -48,4 +49,80 @@ test("nft contract: nft metadata view", async t => {
         symbol: "GOTEAM",
     };
     t.deepEqual(await nft_contract.view("nft_metadata", { account_id: root }), expected);
+});
+
+test("nft contract: nft mint call", async t => {
+    const { alice, nft_contract } = t.context.accounts;
+    const request_payload = {
+        token_id: "TEST123",
+        metadata: {
+            title: "LEEROYYYMMMJENKINSSS",
+            description: "Alright time's up, let's do this.",
+            media:
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Fhp4lHufCdTzTeGCAblOdgHaF7%26pid%3DApi&f=1",
+        },
+        receiver_id: alice,
+    };
+    await alice.call(nft_contract, "nft_mint", request_payload, defaultCallOptions());
+
+    const tokens = await nft_contract.view("nft_tokens");
+    const expected = [
+        {
+            approved_account_ids: {},
+            metadata: {
+                copies: null,
+                description: "Alright time's up, let's do this.",
+                expires_at: null,
+                extra: null,
+                issued_at: null,
+                media:
+                    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Fhp4lHufCdTzTeGCAblOdgHaF7%26pid%3DApi&f=1",
+                media_hash: null,
+                reference: null,
+                reference_hash: null,
+                starts_at: null,
+                title: "LEEROYYYMMMJENKINSSS",
+                updated_at: null,
+            },
+            owner_id: alice.accountId,
+            royalty: {},
+            token_id: "TEST123",
+        },
+    ];
+    t.deepEqual(tokens, expected, "Expected to find one minted NFT");
+});
+
+test("nft contract: nft approve call", async t => {
+    const { alice, nft_contract, market_contract } = t.context.accounts;
+    await mintNFT(alice, nft_contract);
+    await approveNFT(market_contract, alice, nft_contract);
+
+    // test if approved
+    const view_payload = {
+        token_id: "TEST123",
+        approved_account_id: market_contract,
+    };
+    const approved = await nft_contract.view("nft_is_approved", view_payload);
+    test.true(approved, "Failed to approve NFT");
+});
+
+test("nft contract: nft approve call long msg string", async (test, { nft_contract, market_contract, alice }) => {
+    await mintNFT(alice, nft_contract);
+
+    // approve NFT
+    const approve_payload = {
+        token_id: "TEST123",
+        account_id: market_contract,
+        msg: "sample message".repeat(10 * 1024),
+    };
+    const result = await alice.call_raw(nft_contract, "nft_approve", approve_payload, defaultCallOptions());
+    test.regex(result.promiseErrorMessages.join("\n"), /Exceeded the prepaid gas+/);
+
+    // test if approved
+    const view_payload = {
+        token_id: "TEST123",
+        approved_account_id: market_contract,
+    };
+    const approved = await nft_contract.view("nft_is_approved", view_payload);
+    test.false(approved, "NFT approval should have failed");
 });
