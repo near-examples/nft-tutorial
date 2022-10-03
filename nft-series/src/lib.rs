f322c3892c4098037aada6cc82993257060c99b2
BenKurrek <bkurrek@uwaterloo.ca>
2022-10-03 09:28:30 -0400

    removed prefix from standard version

--- nft-series/src/lib.rs
+++ nft-series/src/lib.rs
@@ -28,7 +28,7 @@ mod royalty;
 mod series;
 
 /// This spec can be treated like a version of the standard.
-pub const NFT_METADATA_SPEC: &str = "nft-1.0.0";
+pub const NFT_METADATA_SPEC: &str = "1.0.0";
 /// This is the name of the NFT standard we're using
 pub const NFT_STANDARD_NAME: &str = "nep171";
 
