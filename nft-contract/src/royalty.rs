1b6c738265c64ed3461faa9d4da9b352f3c55ee3
garikbesson <garikbesson@gmail.com>
2024-02-13 15:29:24 +0100

    style fixes

--- nft-contract/src/royalty.rs
+++ nft-contract/src/royalty.rs
@@ -18,14 +18,13 @@ pub trait NonFungibleTokenCore {
 
 #[near_bindgen]
 impl NonFungibleTokenCore for Contract {
-
     //calculates the payout for a token given the passed in balance. This is a view method
     fn nft_payout(&self, token_id: TokenId, balance: U128, max_len_payout: u32) -> Payout {
-		/*
+        /*
             FILL THIS IN
         */
         todo!(); //remove once code is filled in.
-	}
+    }
 
     //transfers the token to the receiver ID and returns the payout object that should be payed given the passed in balance. 
     #[payable]
